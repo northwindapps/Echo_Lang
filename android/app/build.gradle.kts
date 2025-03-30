a9eebc2fb69ca5196c1941b1da1f8027e3c543ab
yujin <northwindapps@gmail.com>
2025-03-30 21:26:15 +0900

    uploaded to google play

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,9 @@
+import java.util.Properties
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(keystorePropertiesFile.inputStream())
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +12,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.test_flutter"
+    namespace = "com.echolang.test_flutter"
     //namespace = "com.tekartik.flutter_background_service"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
@@ -22,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.test_flutter"
+        applicationId = "com.echolang.test_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
@@ -33,13 +39,24 @@ android {
 
     
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    android {
+        signingConfigs {
+            create("release") {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+        buildTypes {
+            getByName("release") {
+                signingConfig = signingConfigs.getByName("release")
+                isMinifyEnabled = false
+                isShrinkResources = false
+            }
         }
     }
+
 }
 
 flutter {
--- android/app/src/main/kotlin/com/echolang/test_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/echolang/test_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.test_flutter
+package com.echolang.test_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
