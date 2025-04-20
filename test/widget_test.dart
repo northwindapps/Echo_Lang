ef5ea789eae6c8c561a8d50d5c68ade452675cc8
yujin <northwindapps@gmail.com>
2025-04-20 11:10:37 +0900

    added dialog jsons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_flutter/main_background.dart';
+// import 'package:test_flutter/main_background.dart';
 
 void main() {
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
